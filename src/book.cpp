51c8e78c87fe5c3253c748c41fe9b926ac6fee6d
Dev <100156709+Dev071998@users.noreply.github.com>
2025-04-26 18:37:54 +0000

    commit added file

---
+++ src/book.cpp
@@ -0,0 +1,8 @@
+#include "/home/g2023ht65065/LibrarySystem/include/book.hpp"
+
+int main()
+{
+    class Book a("devd", "dev");
+    std::string n = a.getTitle();
+    std::cout << n << std::endl;
+}
